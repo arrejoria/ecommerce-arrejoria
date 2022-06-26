25341e86165c635834a20b95e23001dd3b048347
arrejoria <arrejoria1994@gmail.com>
2022-06-25 21:14:56 -0300

    Notificacion producto removido added

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import CartContext from "../../Context/CartContext";
 import './CartItem.css'
-// const CartItem = ({id, name, price, quantity }) => {
+
+import { useNotification } from "../../Notifications/Notification";
+
+
     const CartItem = ({item}) => {
     const { removeItem } = useContext(CartContext)
 
+    const showNotification = useNotification();
+
     const remove = () => {
+
+        showNotification('Producto eliminado del carrito', 'error')
         removeItem(item.id)
     }
 
